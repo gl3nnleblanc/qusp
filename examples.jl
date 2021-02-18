f0df05bf0280ecdf44688081c61a0e0647918bab
Glenn LeBlanc <gleblanc@berkeley.edu>
2021-02-18 14:08:18 -0800

    Add initial skeleton and examples

---
+++ examples.jl
@@ -0,0 +1,7 @@
+using LinearAlgebra
+
+A = [[1 2 3; 4 1 6; 7 8 1]; [1 2 3; 1 2 3; 1 2 3]]
+
+B = rand(4,4,4)
+Btilda = reshape(B,4,4^2)
+
---
+++ tensornetwork.jl
@@ -0,0 +1,13 @@
+using LinearAlgebra
+
+abstract type Network
+    # TODO
+end
+
+abstract type Node
+    # TODO
+end
+
+abstract type Edge
+    # TODO
+end
