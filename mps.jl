319c7a024f2681d05e780e25f94c69de9ca2dc1e
Glenn LeBlanc <gleblanc@berkeley.edu>
2021-02-18 15:39:05 -0800

    D -> rank for clarity

--- mps.jl
+++ mps.jl
@@ -2,23 +2,23 @@
 using LinearAlgebra
 using TSVD
 
-D=4
+rank=4
 k=2
 
-A = rand((2 for _=1:D)...)
+A = rand((2 for _=1:rank)...)
 sites = []
 
 next = []
-A_new = reshape(A, 2^(D-1), 2^1)
+A_new = reshape(A, 2^(rank-1), 2^1)
 next, s, V = TSVD.tsvd(A_new, k)
 push!(sites, V)
 
-A_new = reshape(next, 2^(D-2), 2^2)
+A_new = reshape(next, 2^(rank-2), 2^2)
 next, s, V = TSVD.tsvd(A_new, k)
 push!(sites, reshape(V, 2, 2, 2))
 
-A_new = reshape(next, 2^(D-3), 2^2)
+A_new = reshape(next, 2^(rank-3), 2^2)
 next, s, V = TSVD.tsvd(A_new, k)
 push!(sites, reshape(V, 2, 2, 2))
 
-print(sites)
+# etc...
