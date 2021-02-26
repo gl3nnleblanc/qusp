eb9c405cfb19d9a2f5507204763f2b6b8e64c4d1
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-02-25 16:54:54 -0800

    General mps generator

--- examples.jl
+++ examples.jl
@@ -1,7 +1,30 @@
 using LinearAlgebra
+using TSVD
 
+# Basic Linear Algebra
 A = [[1 2 3; 4 1 6; 7 8 1]; [1 2 3; 1 2 3; 1 2 3]]
 
 B = rand(4,4,4)
 Btilda = reshape(B,4,4^2)
 
+# MPS
+rank=4
+k=2
+
+A = rand((2 for _=1:rank)...)
+sites = []
+
+next = []
+A_new = reshape(A, 2^(rank-1), 2^1)
+next, s, V = TSVD.tsvd(A_new, k)
+push!(sites, V)
+
+A_new = reshape(next, 2^(rank-2), 2^2)
+next, s, V = TSVD.tsvd(A_new, k)
+push!(sites, reshape(V, 2, 2, 2))
+
+A_new = reshape(next, 2^(rank-3), 2^2)
+next, s, V = TSVD.tsvd(A_new, k)
+push!(sites, reshape(V, 2, 2, 2))
+
+# etc...
--- mps.jl
+++ mps.jl
@@ -2,23 +2,29 @@
 using LinearAlgebra
 using TSVD
 
-rank=4
-k=2
-
-A = rand((2 for _=1:rank)...)
-sites = []
-
-next = []
-A_new = reshape(A, 2^(rank-1), 2^1)
-next, s, V = TSVD.tsvd(A_new, k)
-push!(sites, V)
-
-A_new = reshape(next, 2^(rank-2), 2^2)
-next, s, V = TSVD.tsvd(A_new, k)
-push!(sites, reshape(V, 2, 2, 2))
-
-A_new = reshape(next, 2^(rank-3), 2^2)
-next, s, V = TSVD.tsvd(A_new, k)
-push!(sites, reshape(V, 2, 2, 2))
-
-# etc...
+function mps(A, bond_dim=2)
+    # Returns a matrix product state representation of A.
+    # TODO: wrapper class to replace SITES supporting contraction with other tensor
+    sites = []
+    next = []
+    rank = length(size(A))
+    if rank<2
+        return A
+    end
+    A_new = reshape(A, 2^(rank-1), 2^1)
+    next, s, V = TSVD.tsvd(A_new, bond_dim)
+    next = next * diagm(s)
+    push!(sites, V)
+    for i=2:rank-2
+        print(i)
+        A_new = reshape(next, 2^(rank-i), 2^2)
+        next, s, V = TSVD.tsvd(A_new, bond_dim)
+        next = next * diagm(s)
+        push!(sites, reshape(V, 2, 2, 2))
+    end
+    A_new = reshape(next, 2, 2^2)
+    next, s, V = TSVD.tsvd(A_new, bond_dim)
+    push!(sites, V)
+    #push!(next * diagm(s))
+    return sites
+ end
