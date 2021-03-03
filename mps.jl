b988b364be2704905268f201b2bb70a60988e77f
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-03-03 13:33:57 -0800

    tsvd->svd to fix weird behavior

--- mps.jl
+++ mps.jl
@@ -5,16 +5,34 @@ export mps
 using LinearAlgebra
 using TSVD
 
+"""
+    truncate(s, dim)
+
+Zero entries of `s` on indices greater than `dim`.
+"""
+function truncate(s, dim)
+    for (i,_)=enumerate(s)
+        if i > dim
+            s[i]=0
+        end
+    end
+    return s
+end
+
+
+"""
+    mps(A, bond_dim)
+
+Generate a matrix product state representation of the tensor `A`, using bond
+dimension `bond_dim`. Limited to tensors having equal dimension on each index.
+"""
 function mps(A, bond_dim=2)
-    # Limited to tensors with each index having equal dimension
     for axis_dimension in size(A)
         if axis_dimension != size(A)[1]
             throw(DomainError(size(A), "mps is not supported for tensors hav" *
                               "ing nonuniform dimensions"))
         end
     end
-    # Returns a matrix product state representation of A.
-    #  A |-> [M_1, M_2, ... , M_r]
     # TODO: wrapper class to replace SITES supporting contraction with other
     #   tensor
     # TODO: support choosing left/right canonical form
@@ -25,18 +43,21 @@ function mps(A, bond_dim=2)
         return A
     end
     A_new = reshape(A, 2^(rank-1), 2^1)
-    next, s, V = TSVD.tsvd(A_new, bond_dim)
+    next, s, V = svd(A_new)
+    s = truncate(s, bond_dim)
     next = next * diagm(s)
     push!(sites, V)
     for i=2:rank-2
         print(i)
         A_new = reshape(next, 2^(rank-i), 2^2)
-        next, s, V = TSVD.tsvd(A_new, bond_dim)
+        next, s, V = svd(A_new)
+        s = truncate(s, bond_dim)
         next = next * diagm(s)
         push!(sites, reshape(V, 2, 2, 2))
     end
     A_new = reshape(next, 2, 2^2)
-    next, s, V = TSVD.tsvd(A_new, bond_dim)
+    next, s, V = svd(A_new)
+    s = truncate(s, bond_dim)
     push!(sites, V)
     push!(sites, next * diagm(s))
     return sites
--- mps_test.jl
+++ mps_test.jl
@@ -1,3 +1,4 @@
+using LinearAlgebra
 using Test
 
 include("./mps.jl")
@@ -5,7 +6,7 @@ using .MatrixProductState
 
 @testset "Matrix Product State Tests" begin
     @testset "Error Handling" begin
-        A = rand(2, 2, 2, 2, 4)
+        A = rand(1, 2)
         err = nothing
         try mps(A)
             catch err
@@ -13,9 +14,33 @@ using .MatrixProductState
         @test err isa DomainError
     end
     @testset "Contraction" begin
-        A = rand((2 for _=1:10)...)
+        # First simple test
+        A = ones(2,2,2)
+        A[1,1,1] = 0
+        A[1,2,2] = 0
+        A[2,1,2] = 0
+        A[2,2,1] = 0
         sites = mps(A)
-        # TODO: contract and test mps(A) == A
-        @test A != mps(A)
+        intermediate = transpose(reshape(sites[2], 2, 4)) * sites[3]
+        intermediate = reshape(intermediate, 2, 2, 2)
+        intermediate = sites[1] * reshape(intermediate, 2, 4)
+        A_mps = reshape(intermediate, 2, 2, 2)
+        @test abs(dot(A, A_mps) - 4) < 1e-6
+
+        # Second simple test
+        A = zeros(2,2,2)
+        A[1,1,1] = -1
+        A[1,2,2] = 1
+        A[2,1,2] = -1
+        A[2,2,1] = 1
+        sites = mps(A)
+        intermediate = transpose(reshape(sites[2], 2, 4)) * sites[3]
+        intermediate = reshape(intermediate, 2, 2, 2)
+        intermediate = sites[1] * reshape(intermediate, 2, 4)
+        A_mps = reshape(intermediate, 2, 2, 2)
+        print(A_mps)
+        print("\n\n\n")
+        print(A)
+        @test abs(dot(A, A_mps) - 20) < 1e-6
     end
 end
