5a4e94856df26a5a2c77e2c25a7a108f00a5e3d5
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-03-06 23:14:11 -0800

    Format

--- mps.jl
+++ mps.jl
@@ -32,11 +32,15 @@ end
 Generate a matrix product state representation of the tensor `A`, using bond
 dimension `bond_dim`. Limited to tensors having equal dimension on each index.
 """
-function mps(A, bond_dim=2)
+function mps(A, bond_dim = 2)
     for axis_dimension in size(A)
         if axis_dimension != size(A)[1]
-            throw(DomainError(size(A), "mps is not supported for tensors hav" *
-                              "ing nonuniform dimensions"))
+            throw(
+                DomainError(
+                    size(A),
+                    "mps is not supported for tensors hav" * "ing nonuniform dimensions",
+                ),
+            )
         end
     end
     # TODO: wrapper class to replace SITES supporting contraction with other
@@ -48,19 +52,19 @@ function mps(A, bond_dim=2)
     if rank < 2
         return A
     end
-    A_new = reshape(A, 2^(rank-1), 2^1)
+    A_new = reshape(A, 2^(rank - 1), 2^1)
     next, s, V = svd(A_new)
     s = truncate_and_renormalize(s, bond_dim)
     next = next * diagm(s)
     push!(sites, transpose(V))
-    next_axis_dim=length(s)
-    for i=2:rank-2
-        A_new = reshape(next, 2^(rank-i), next_axis_dim*2)
+    next_axis_dim = length(s)
+    for i = 2:rank-2
+        A_new = reshape(next, 2^(rank - i), next_axis_dim * 2)
         next, s, V = svd(A_new)
         s = truncate_and_renormalize(s, bond_dim)
         V = transpose(V)
-        V = V[1:(size(V)[1]<bond_dim ? end : bond_dim),:]
-        next = next[:,1:(size(next)[2]<bond_dim ? end : bond_dim)] * diagm(s)
+        V = V[1:(size(V)[1] < bond_dim ? end : bond_dim), :]
+        next = next[:, 1:(size(next)[2] < bond_dim ? end : bond_dim)] * diagm(s)
         next_axis_dim = 0
         if length(V) < bond_dim^2 * 2
             prev_axis_dim = size(sites[i-1])[1]
@@ -72,7 +76,7 @@ function mps(A, bond_dim=2)
         end
     end
     if bond_dim > 3
-        A_new = reshape(next, 2, 2^(next_axis_dim-1))
+        A_new = reshape(next, 2, 2^(next_axis_dim - 1))
     else
         A_new = reshape(next, 2, 4)
     end
@@ -82,6 +86,6 @@ function mps(A, bond_dim=2)
     push!(sites, reshape(V, 2, 2, length(V) ÷ 4))
     push!(sites, next * diagm(s))
     return sites
- end
+end
 
- end # module
+end # module
--- mps_test.jl
+++ mps_test.jl
@@ -8,30 +8,31 @@ using .MatrixProductState
     @testset "Error Handling" begin
         A = rand(1, 2)
         err = nothing
-        try mps(A)
-            catch err
+        try
+            mps(A)
+        catch err
         end
         @test err isa DomainError
     end
     @testset "Contraction" begin
         # First simple test
-        A = ones(2,2,2)
-        A[1,1,1] = 0
-        A[1,2,2] = 0
-        A[2,1,2] = 0
-        A[2,2,1] = 0
-        A[1,1,2] = 2
+        A = ones(2, 2, 2)
+        A[1, 1, 1] = 0
+        A[1, 2, 2] = 0
+        A[2, 1, 2] = 0
+        A[2, 2, 1] = 0
+        A[1, 1, 2] = 2
         sites = mps(A)
         intermediate = reshape(sites[2], 4, 2) * sites[1]
         A_mps = reshape(sites[3] * reshape(intermediate, 2, 4), 2, 2, 2)
         @test abs(dot(A, A_mps) / dot(A, A) - 1) < 1e-7
 
         # Second simple test
-        A = zeros(2,2,2)
-        A[1,1,1] = -1
-        A[1,2,2] = 1
-        A[2,1,2] = -1
-        A[2,2,1] = 1
+        A = zeros(2, 2, 2)
+        A[1, 1, 1] = -1
+        A[1, 2, 2] = 1
+        A[2, 1, 2] = -1
+        A[2, 2, 1] = 1
         sites = mps(A)
         intermediate = reshape(sites[2], 4, 2) * sites[1]
         A_mps = reshape(sites[3] * reshape(intermediate, 2, 4), 2, 2, 2)
@@ -42,19 +43,20 @@ using .MatrixProductState
         #A = zeros((2 for _=1:rank)...)
         #A[(1 for _=1:rank)...] = 1
         #A[(2 for _=1:rank)...] = -1
-        A = rand((2 for _=1:rank)...)
+        A = rand((2 for _ = 1:rank)...)
         A = A / sqrt(dot(A, A))
         sites = mps(A, 512)
         axis_dim = div(length(sites[2]), 2)
         intermediate = reshape(sites[2], axis_dim, 2) * sites[1]
-        for i=2:rank-2
+        for i = 2:rank-2
             axis_dim = size(sites[i+1])[3]
             left_axis_dim = div(length(sites[i+1]), axis_dim)
             right_axis_dim = div(length(intermediate), axis_dim)
-            intermediate = reshape(sites[i+1], left_axis_dim, axis_dim) *
+            intermediate =
+                reshape(sites[i+1], left_axis_dim, axis_dim) *
                 reshape(intermediate, axis_dim, right_axis_dim)
         end
-        A_mps = sites[rank] * reshape(intermediate, 2, 2^(rank-1))
+        A_mps = sites[rank] * reshape(intermediate, 2, 2^(rank - 1))
         @test abs(dot(A_mps, A) - 1) < 1e-7
     end
 end
