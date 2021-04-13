54358d18b1d297141567ec0da414f8bbc757fe5c
Glenn LeBlanc <g.leblanc135@gmail.com>
2021-04-13 14:25:14 -0700

    Passes preliminary test using Hamiltonian input

--- tebd.jl
+++ tebd.jl
@@ -24,12 +24,6 @@ function block_evolve(ψ::MPS, H::Hamiltonian, t::Number)
     interaction = reshape(exp(H.interaction * t), 2, 2, 2, 2)
     field = exp(H.field * t)
     half_field = exp(H.field * t / 2)
-    G = zeros(2, 2, 2, 2)
-    G[1, 1, 1, 1] = 1
-    G[2, 2, 1, 1] = 1
-    G[2, 2, 2, 2] = 1
-    G[1, 1, 2, 2] = 1
-    #@einsum res[q1, q2] := G[a, q1, c, q2] * ψ1[a] * ψ2[c]
     N = length(ψ.sites)
     # Left to Right
     local previous_right_site::Array{<:Number}
@@ -45,7 +39,11 @@ function block_evolve(ψ::MPS, H::Hamiltonian, t::Number)
 
         if i == N - 1
             @einsum block[q1, q2, right] := left_site[q1, chi] * right_site[chi, q2, right]
-            @einsum evolved[q1, q2, right] := block[a, b, right] * G[a, q1, b, q2]
+            @einsum evolved[q1, q2, right] :=
+                interaction[q1, q2, d, c] *
+                field[c, a] *
+                half_field[d, b] *
+                block[a, b, right]
             _, new_left_site, new_right_site =
                 split_tensor(evolved, 2, 2 * ψ.bond_dim, ψ.bond_dim)
             push!(updated_sites, new_left_site)
@@ -55,7 +53,10 @@ function block_evolve(ψ::MPS, H::Hamiltonian, t::Number)
             @einsum block[left, q1, q2, right] :=
                 left_site[left, q1, chi] * right_site[chi, q2, right]
             @einsum evolved[left, q1, q2, right] :=
-                block[left, a, b, right] * G[a, q1, b, q2]
+                interaction[q1, q2, d, c] *
+                half_field[c, a] *
+                half_field[d, b] *
+                block[left, a, b, right]
             _, new_left_site, new_right_site =
                 split_tensor(evolved, 2 * ψ.bond_dim, 2 * ψ.bond_dim, ψ.bond_dim)
             push!(
@@ -71,7 +72,11 @@ function block_evolve(ψ::MPS, H::Hamiltonian, t::Number)
 
         elseif i == 1
             @einsum block[left, q1, q2] := left_site[left, q1, chi] * right_site[chi, q2]
-            @einsum evolved[left, q1, q2] := block[left, a, b] * G[a, q1, b, q2]
+            @einsum evolved[left, q1, q2] :=
+                interaction[q1, q2, d, c] *
+                half_field[c, a] *
+                field[d, b] *
+                block[left, a, b]
             _, new_left_site, new_right_site =
                 split_tensor(evolved, 2 * ψ.bond_dim, 2, ψ.bond_dim)
             push!(updated_sites, reshape(new_left_site, size(left_site)...))
